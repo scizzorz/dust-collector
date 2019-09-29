db861ce2f592ab0c5b7e83c6d215e5687d294b45
John Weachock <jweachock@gmail.com>
2019-09-29 18:14:20 -0400

    Add OOP because everything is better with OOP

--- main.cpp
+++ main.cpp
@@ -7,40 +7,63 @@
 #define OPEN true
 #define CLOSED false
 
-Servo gates[NUM_GATES];
-int servoPins[NUM_GATES] = {11, 10, 9, 6, 5, 3};
-int buttonPins[NUM_GATES] = {13, 12, 8, 7, 4, 2};
-int openPosition[NUM_GATES] = {40, 45, 80, 80, 80, 80};
-int closePosition[NUM_GATES] = {115, 120, 100, 100, 100, 100};
-bool status[NUM_GATES] = {CLOSED, CLOSED, CLOSED, CLOSED, CLOSED, CLOSED};
+class Gate {
+public:
+  int buttonPin;
+  int servoPin;
+  int openPosition;
+  int closePosition;
+  bool status;
+  Servo servo;
 
-void setGate(int i, bool open) {
-  if (open != status[i]) {
-    gates[i].write(open ? openPosition[i] : closePosition[i]);
-    status[i] = open;
-    delay(MOVETIME);
+  Gate(int buttonPin, int servoPin, int openPosition, int closePosition)
+      : buttonPin(buttonPin), servoPin(servoPin), openPosition(openPosition),
+        closePosition(closePosition) {
+    this->status = CLOSED;
   }
-}
+
+  void init() {
+    pinMode(this->buttonPin, INPUT);
+    this->servo.attach(this->servoPin);
+    this->servo.write(this->closePosition);
+  }
+
+  void open() { this->set(true); }
+
+  void close() { this->set(false); }
+
+  void set(bool open) {
+    if (open != this->status) {
+      this->servo.write(open ? this->openPosition : this->closePosition);
+      this->status = open;
+      delay(MOVETIME);
+    }
+  }
+
+  bool isPressed() { return digitalRead(this->buttonPin); }
+};
+
+Gate gates[NUM_GATES] = {
+    Gate(13, 11, 40, 115), Gate(12, 10, 45, 120), Gate(8, 9, 80, 100),
+    Gate(7, 6, 80, 100),   Gate(4, 5, 80, 100),   Gate(2, 3, 80, 100),
+};
 
 void setup() {
   for (int i = 0; i < NUM_GATES; i++) {
-    pinMode(buttonPins[i], INPUT);
-    gates[i].attach(servoPins[i]);
-    gates[i].write(closePosition[i]);
+    gates[i].init();
     delay(500);
-    setGate(i, OPEN);
-    setGate(i, CLOSED);
+    gates[i].open();
+    gates[i].close();
   }
 }
 
 void loop() {
   for (int i = 0; i < NUM_GATES; i++) {
-    int pressed = digitalRead(buttonPins[i]);
-    if (pressed) {
-      setGate(i, OPEN);
+    if (gates[i].isPressed()) {
+      gates[i].open();
       for (int j = 0; j < NUM_GATES; j++) {
         if (i != j) {
-          setGate(j, CLOSED);
+          gates[j].close();
         }
       }
     }
