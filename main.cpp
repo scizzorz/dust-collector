f34b86140920d7ba7bc2983783098bb3dfb0c33a
John Weachock <jweachock@gmail.com>
2019-09-29 17:17:18 -0400

    Add autocalibration - kidding, servos have no feedback

--- main.cpp
+++ main.cpp
@@ -2,24 +2,68 @@
 #include <Servo.h>
 
 #define NUM_GATES 6
-#define CLOSED 180
-#define OPEN 115
-#define MOVETIME 1000
+#define MOVETIME 2000
+#define OPEN true
+#define CLOSED false
+#define NEUTRAL 90
 
 Servo gates[NUM_GATES];
 int servoPins[NUM_GATES] = {11, 10, 9, 6, 5, 3};
 int buttonPins[NUM_GATES] = {13, 12, 8, 7, 4, 2};
-int openGate = 0;
-int open = false;
+int openPosition[NUM_GATES] = {0, 0, 0, 0, 0, 0};
+int closePosition[NUM_GATES] = {180, 180, 180, 180, 180, 180};
+bool status[NUM_GATES] = {CLOSED, CLOSED, CLOSED, CLOSED, CLOSED, CLOSED};
+
+void setGate(int i, bool open) {
+  if(open != status[i]) {
+    if(open) {
+      gates[i].write(openPosition[i]);
+    } else {
+      gates[i].write(closePosition[i]);
+    }
+    status[i] = open;
+    delay(MOVETIME);
+  }
+}
+
+int findLimit(int i, bool open) {
+  int adjust;
+  if(open) {
+    adjust = -1; // lower degrees -> more open
+  } else {
+    adjust = 1; // higher degrees -> more closed
+  }
+
+  int target = NEUTRAL;
+  gates[i].write(target);
+  delay(1000);
+
+  while(true) {
+    target += adjust;
+    gates[i].write(target);
+    delay(150);
+    if(target == 0 || target == 180) {
+      return target;
+    }
+    if(gates[i].read() != target) {
+      return target - adjust;
+    }
+  }
+
+  return NEUTRAL;
+}
 
 void setup() {
   for (int i = 0; i < NUM_GATES; i++) {
     gates[i].attach(servoPins[i]);
-    gates[i].write(OPEN);
-    delay(MOVETIME);
-    gates[i].write(CLOSED);
-    delay(MOVETIME);
     pinMode(buttonPins[i], INPUT);
+  }
+
+  for (int i = 0; i < 1; i++) { // FIXME
+    openPosition[i] = findLimit(i, OPEN);
+    closePosition[i] = findLimit(i, CLOSED);
+    setGate(i, OPEN);
+    setGate(i, CLOSED);
   }
 }
 
@@ -29,8 +73,8 @@ void loop() {
     if (pressed) {
       gates[i].write(OPEN);
       delay(MOVETIME);
-      open = true;
-      if (open && openGate != i) {
+      anyOpen = true;
+      if (anyOpen && openGate != i) {
         gates[openGate].write(CLOSED);
         delay(MOVETIME);
       }
