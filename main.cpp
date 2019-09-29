0a5892d583afe6c000408e888d7c6579ed2934d4
John Weachock <jweachock@gmail.com>
2019-09-29 18:00:07 -0400

    General code improvements

--- main.cpp
+++ main.cpp
@@ -2,66 +2,32 @@
 #include <Servo.h>
 
 #define NUM_GATES 6
-#define MOVETIME 2000
+#define MOVETIME 750
+#define NEUTRAL 90
 #define OPEN true
 #define CLOSED false
-#define NEUTRAL 90
 
 Servo gates[NUM_GATES];
 int servoPins[NUM_GATES] = {11, 10, 9, 6, 5, 3};
 int buttonPins[NUM_GATES] = {13, 12, 8, 7, 4, 2};
-int openPosition[NUM_GATES] = {0, 0, 0, 0, 0, 0};
-int closePosition[NUM_GATES] = {180, 180, 180, 180, 180, 180};
+int openPosition[NUM_GATES] = {40, 45, 80, 80, 80, 80};
+int closePosition[NUM_GATES] = {115, 120, 100, 100, 100, 100};
 bool status[NUM_GATES] = {CLOSED, CLOSED, CLOSED, CLOSED, CLOSED, CLOSED};
 
 void setGate(int i, bool open) {
-  if(open != status[i]) {
-    if(open) {
-      gates[i].write(openPosition[i]);
-    } else {
-      gates[i].write(closePosition[i]);
-    }
+  if (open != status[i]) {
+    gates[i].write(open ? openPosition[i] : closePosition[i]);
     status[i] = open;
     delay(MOVETIME);
   }
 }
 
-int findLimit(int i, bool open) {
-  int adjust;
-  if(open) {
-    adjust = -1; // lower degrees -> more open
-  } else {
-    adjust = 1; // higher degrees -> more closed
-  }
-
-  int target = NEUTRAL;
-  gates[i].write(target);
-  delay(1000);
-
-  while(true) {
-    target += adjust;
-    gates[i].write(target);
-    delay(150);
-    if(target == 0 || target == 180) {
-      return target;
-    }
-    if(gates[i].read() != target) {
-      return target - adjust;
-    }
-  }
-
-  return NEUTRAL;
-}
-
 void setup() {
   for (int i = 0; i < NUM_GATES; i++) {
-    gates[i].attach(servoPins[i]);
     pinMode(buttonPins[i], INPUT);
-  }
-
-  for (int i = 0; i < 1; i++) { // FIXME
-    openPosition[i] = findLimit(i, OPEN);
-    closePosition[i] = findLimit(i, CLOSED);
+    gates[i].attach(servoPins[i]);
+    gates[i].write(closePosition[i]);
+    delay(500);
     setGate(i, OPEN);
     setGate(i, CLOSED);
   }
@@ -71,23 +37,22 @@ void loop() {
   for (int i = 0; i < NUM_GATES; i++) {
     int pressed = digitalRead(buttonPins[i]);
     if (pressed) {
-      gates[i].write(OPEN);
-      delay(MOVETIME);
-      anyOpen = true;
-      if (anyOpen && openGate != i) {
-        gates[openGate].write(CLOSED);
-        delay(MOVETIME);
+      setGate(i, OPEN);
+      for (int j = 0; j < NUM_GATES; j++) {
+        if (i != j) {
+          setGate(j, CLOSED);
+        }
       }
-      openGate = i;
     }
   }
+
   delay(10);
 }
 
 int main(void) {
   init();
   setup();
-  for (;;) {
+  while (true) {
     loop();
   }
 }
