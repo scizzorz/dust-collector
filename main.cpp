6ff545efbb18a63836e8afb51594ef4894297421
John Weachock <jweachock@gmail.com>
2019-10-02 20:06:56 -0400

    Add complete collector logic

--- main.cpp
+++ main.cpp
@@ -9,16 +9,15 @@
 #define ON true
 #define OFF false
 #define COLLECTOR_WAIT 300
-
-int collectorOn = A4;
-int collectorOff = A5;
+#define DEBOUNCE 50
 
 class Collector {
-public:
+private:
   int onPin;
   int offPin;
   bool status;
 
+public:
   Collector(int onPin, int offPin)
       : onPin(onPin), offPin(offPin), status(OFF) {}
 
@@ -31,26 +30,35 @@ public:
 
   void off() { this->set(OFF); }
 
+  void toggle() { this->set(!this->status); }
+
   void set(bool to) {
     int pin = to ? this->onPin : this->offPin;
     digitalWrite(pin, HIGH);
     delay(COLLECTOR_WAIT);
     digitalWrite(pin, LOW);
+    this->status = to;
   }
+
+  bool isOn() { return this->status; }
 };
 
 class Gate {
-public:
+private:
+  unsigned long lastDebounce = 0;
+  int lastButtonState = HIGH;
   int buttonPin;
   int servoPin;
   int openPosition;
   int closePosition;
-  bool status;
   Servo servo;
+  bool status = CLOSED;
+  int buttonState = LOW;
 
+public:
   Gate(int buttonPin, int servoPin, int openPosition, int closePosition)
       : buttonPin(buttonPin), servoPin(servoPin), openPosition(openPosition),
-        closePosition(closePosition), status(CLOSED) {}
+        closePosition(closePosition) {}
 
   void init() {
     pinMode(this->buttonPin, INPUT);
@@ -58,9 +66,11 @@ public:
     this->servo.write(this->closePosition);
   }
 
-  void open() { this->set(true); }
+  void open() { this->set(OPEN); }
 
-  void close() { this->set(false); }
+  void close() { this->set(CLOSED); }
+
+  void toggle() { this->set(!this->status); }
 
   void set(bool open) {
     if (open != this->status) {
@@ -70,35 +80,80 @@ public:
     }
   }
 
-  bool isPressed() { return digitalRead(this->buttonPin); }
+  /// This returns true if the button state CHANGED. It does *not* return the
+  /// button state! If you want to react to presses, use
+  /// `.checkPress() && .isPressed()`
+  bool checkPress() {
+    bool change = false;
+    int currentReading = digitalRead(this->buttonPin);
+    if (currentReading != this->lastButtonState) {
+      this->lastDebounce = millis();
+    }
+
+    if ((millis() - this->lastDebounce) > DEBOUNCE) {
+      if (currentReading != this->buttonState) {
+        this->buttonState = currentReading;
+        change = true;
+      }
+    }
+
+    this->lastButtonState = currentReading;
+    return change;
+  }
+
+  bool isPressed() { return this->buttonState; }
+
+  bool isOpen() { return this->status; }
 };
 
+Collector collector(A4, A5);
 Gate gates[NUM_GATES] = {
     Gate(13, 11, 40, 105), Gate(12, 10, 45, 120), Gate(8, 9, 80, 100),
     Gate(7, 6, 80, 100),   Gate(4, 5, 80, 100),   Gate(2, 3, 80, 100),
 };
 
-Collector collector(A4, A5);
+void press(int i) {
+  // if this gate is already open, just toggle the dust collector.
+  if (gates[i].isOpen()) {
+    collector.toggle();
+    return;
+  }
+
+  // if the gate isn't already open, we need to open it.
+  // do this before turning on the dust collector to avoid having no open gates
+  // with the collector on.
+  gates[i].open();
+
+  // turn on the collector if we need to
+  if (!collector.isOn()) {
+    collector.on();
+  }
+
+  // close all other gates. do this after opening the target gate to avoid
+  // having no open gates with the collector on.
+  for (int j = 0; j < NUM_GATES; j++) {
+    if (i != j) {
+      gates[j].close();
+    }
+  }
+}
 
 void setup() {
   collector.init();
   for (int i = 0; i < NUM_GATES; i++) {
     gates[i].init();
+    /*
     delay(500);
     gates[i].open();
     gates[i].close();
+    */
   }
 }
 
 void loop() {
   for (int i = 0; i < NUM_GATES; i++) {
-    if (gates[i].isPressed()) {
-      gates[i].open();
-      for (int j = 0; j < NUM_GATES; j++) {
-        if (i != j) {
-          gates[j].close();
-        }
-      }
+    if (gates[i].checkPress() && gates[i].isPressed()) {
+      press(i);
     }
   }
 
